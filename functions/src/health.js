4bc2fed7834bced7df1df081b3ce2e2e641754f3
Thibaut Tiberghien <thibaut@smplrspace.com>
2019-07-17 15:52:03 +0800

    add firebase cli token env

--- functions/src/health.js
+++ functions/src/health.js
@@ -3,6 +3,6 @@ const uuid = require('uuid/v4')
 export async function handler (event, context) {
   return {
     statusCode: 200,
-    body: `Hello 2 - ${uuid()}`
+    body: `Hello 3 - ${uuid()}`
   }
 }
